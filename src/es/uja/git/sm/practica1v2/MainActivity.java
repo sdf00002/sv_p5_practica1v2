e9b7bd2d2a4f9c295a3976395325bacc830c70c8
sdf00002 <sdf00002@red.ujaen.es>
2015-11-06 12:15:29 +0100

    Modificación fragment_main, nombre de la app y actividad de ayuda.

--- src/es/uja/git/sm/practica1v2/MainActivity.java
+++ src/es/uja/git/sm/practica1v2/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
