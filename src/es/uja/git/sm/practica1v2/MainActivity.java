0e7ff00cdcf739885b7746d56a804df38652471b
sdf00002 <sdf00002@red.ujaen.es>
2015-11-07 12:25:38 +0100

    Modificación layouts varios e inclusión de confirmación de salida.

--- src/es/uja/git/sm/practica1v2/MainActivity.java
+++ src/es/uja/git/sm/practica1v2/MainActivity.java
@@ -1,16 +1,18 @@
 package es.uja.git.sm.practica1v2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -75,7 +77,7 @@ public class MainActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			
+			//Almacenamos en variables los elementos de la actividad principal
 			View lanzar = rootView.findViewById(R.id.boton1);
 		    final View usuario=rootView.findViewById(R.id.usuario);
 		    final View password=rootView.findViewById(R.id.password);
@@ -87,7 +89,7 @@ public class MainActivity extends Activity {
 				@Override
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
-				
+					//Comprobamos que ninguno de los campos este vacío y hay alguno mandamos un toast indicando que los datos están incompletos
 					if(((EditText)usuario).getText().toString().equals("")||((EditText)password).getText().toString().equals("")||((EditText)ip).getText().toString().equals("")||((EditText)port).getText().toString().equals(""))
 					{
 						
@@ -101,6 +103,7 @@ public class MainActivity extends Activity {
 					} else { 
 					Intent intent= new Intent();
 					intent.setClass(getActivity(), SecondActivity.class);
+					//Pasamos los valores de los elementos a la siguiente actividad
 					intent.putExtra("user", ((EditText)usuario).getText().toString());
 					intent.putExtra("pass", ((EditText)password).getText().toString());
 					intent.putExtra("ip", ((EditText)ip).getText().toString());
@@ -111,7 +114,34 @@ public class MainActivity extends Activity {
 			});
 		
 			return rootView;
-		}
+		}		 
 
 	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	   
+	  if (keyCode == KeyEvent.KEYCODE_BACK) {
+	   
+	    new AlertDialog.Builder(this)
+	      .setIcon(android.R.drawable.ic_dialog_alert)
+	      .setTitle("Salir")
+	      .setMessage("¿Estás seguro?")
+	      .setNegativeButton(android.R.string.cancel, null)//sin listener
+	      .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+	        @Override
+	        public void onClick(DialogInterface dialog, int which){
+	          //Salir
+	          MainActivity.this.finish();
+	        }
+	      })
+	      .show();
+
+	    // Si el listener devuelve true, significa que el evento esta procesado, y nadie debe hacer nada mas
+	    return true;
+	  }
+	//para las demas cosas, se reenvia el evento al listener habitual
+	  return super.onKeyDown(keyCode, event);
+	} 
+
 }
