70bbf400f377b0c976eb3f1248b16786f6281590
sdf00002 <sdf00002@red.ujaen.es>
2015-12-02 11:55:07 +0100

    Eliminación de clases relacionadas con base de datos.
    Inclusión de clases para almacenamiento en ficheros. Modificación del
    layout de la segunda actividad.

--- src/es/uja/git/sm/practica1v2/SecondActivity.java
+++ src/es/uja/git/sm/practica1v2/SecondActivity.java
@@ -1,19 +1,29 @@
 package es.uja.git.sm.practica1v2;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import es.uja.git.sv.examples.Network;
+import es.uja.git.sv.examples.R;
+import es.uja.git.sv.examples.Network.SocketConnection;
 
 public class SecondActivity extends Activity {
 	
@@ -50,7 +60,9 @@ public class SecondActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.second_activity);
 		TextView panel = (TextView)findViewById(R.id.textView1);
-		panel.setText(conectaSocket(ip, puerto));
+		//panel.setText(conectaSocket(ip, puerto));
+		//SocketConnection task = new SocketConnection();
+		//task.execute(url);
 		/**
 		//Asociamos las variables tipo TextView a los campos de la interfaz
 		TextView user = (TextView)findViewById(R.id.textView1);
@@ -108,5 +120,162 @@ public class SecondActivity extends Activity {
 		return "Conexión fallida";
 
 	}
+	
+	private class SocketConnection extends AsyncTask<URL, String, String> {
+		ProgressDialog pbar = null;
+
+		@Override
+		protected String doInBackground(URL... urls) {
+
+			// params comes from the execute() call: params[0] is the url.
+
+			return conectaSocket(urls[0]);
+
+		}
+
+		@Override
+		protected void onPreExecute() {
+
+			pbar = new ProgressDialog(Network.this);
+			pbar.setIndeterminate(true);
+			pbar.setMessage(getBaseContext().getString(R.string.socket_downloading));
+
+			pbar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			pbar.setCancelable(false);
+			if (!pbar.isShowing()) {
+				pbar.show();
+			}
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(String... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		protected void onPostExecute(final String result) {
+			web.setText(result);
+			if (pbar != null)
+				pbar.dismiss();
+
+		}
+
+	}
+	
+	public String getFecha(){
+		String dia,mes,annio;
+		Calendar c = new GregorianCalendar();
+		 dia = Integer.toString(c.get(Calendar.DATE));
+		   if(dia.length()==1)
+			   dia="0"+dia;
+		   mes = Integer.toString(c.get(Calendar.MONTH)+1);
+		   if(mes.length()==1)
+			   mes="0"+mes;
+		   annio = Integer.toString(c.get(Calendar.YEAR));
+		   return annio+"-"+mes+"-"+dia;
+		
+	}
+	
+	public void onSave(View v) {
+		int value;
+		
+		EditText edit_u = (EditText) findViewById(R.id.ficheros_usuario);
+		EditText edit_o = (EditText) findViewById(R.id.ficheros_operacion);
+		EditText edit_v = (EditText) findViewById(R.id.ficheros_value);
+
+		String usuario = edit_u.getEditableText().toString();
+		String operacion = edit_o.getEditableText().toString();
+		String valor = edit_v.getEditableText().toString();
+		
+		String filename = usuario;
+
+		if (valor != null)
+			try {
+				value = Integer.valueOf(valor);
+			} catch (NumberFormatException es) {
+				value = 0;
+			}
+		else
+			value = 0;
+
+         
+		try {
+			FileOutputStream os = openFileOutput(filename, MODE_PRIVATE | MODE_APPEND);
+			/* Escritura sin serialización*/
+			DataOutputStream dos = new DataOutputStream(os);
+			dos.writeUTF(operacion);
+			dos.writeInt(value);
+			dos.flush();
+			dos.close();
+
+			/*Escritura con serialización*/
+//			Record data = new Record(tag,value);
+//			ObjectOutputStream oos = new ObjectOutputStream(os);
+//			oos.writeObject(data);
+//			oos.flush();
+//			oos.close();
+			/*Fin escritura con serialización*/
+			os.close();
+			Toast.makeText(this,
+					getResources().getString(R.string.toast_saved),
+					Toast.LENGTH_SHORT).show();
+
+
+		} catch (IOException ex) {
+			Toast.makeText(this,
+					getResources().getString(R.string.toast_error),
+					Toast.LENGTH_SHORT).show();
+		    
+		}
+		
+     
+	}
+	
+	public void onLoad(View v) {
+
+		String texto = "";
+
+		try {
+
+			EditText edit_u = (EditText) findViewById(R.id.ficheros_usuario);
+			
+			TextView resultado = (TextView) findViewById(R.id.textView1);
+			
+			String filename = edit_u.getEditableText().toString();
+
+			FileInputStream is = openFileInput(filename);
+			
+			/* Lectura sin serialización */
+			DataInputStream dos = new DataInputStream(is);
+			
+			int n = dos.available();
+			texto = "Leidos (" + n + " bytes)\r\n";
+			while (dos.available() > 0) {
+				texto = texto + " clave: " + dos.readUTF() + " valor:"
+						+ dos.readInt() + "\r\n";
+			}
+			dos.close();	
+
+					
+			is.close();
+			resultado.setText(texto);
+
+			
+
+			Toast.makeText(this,
+					getResources().getString(R.string.toast_loaded),
+					Toast.LENGTH_SHORT).show();
+
+		} catch (IOException ex) {
+			Toast.makeText(this,
+					getResources().getString(R.string.toast_error),
+					Toast.LENGTH_SHORT).show();
+
+		}
+
+	}
+	
 
 }
